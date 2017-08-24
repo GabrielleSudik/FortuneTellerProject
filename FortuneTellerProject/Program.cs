bc7457b3bb61767ea940a4fba8989d4513ff1357
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-08-24 19:21:23 -0400

    project should be final

--- FortuneTellerProject/Program.cs
+++ FortuneTellerProject/Program.cs
@@ -14,23 +14,60 @@ namespace FortuneTellerProject
             //start with getting basic info
 
             Console.WriteLine("I am the Great GatlantisS and I will read your fortune!\n");
+            Console.WriteLine("If you get scared at any point, type \"quit\" and your reading will end.");
 
             Console.WriteLine("What is your first name?");
             string firstName = Console.ReadLine();
 
+            if (firstName.ToLower() == "quit")
+            {
+                Console.WriteLine("Nobody likes a quitter!");
+                return;
+            }
+
             Console.WriteLine("What is your last name?");
             string lastName = Console.ReadLine();
 
+            if (lastName.ToLower() == "quit")
+            {
+                Console.WriteLine("Nobody likes a quitter!");
+                return;
+            }
+
             Console.WriteLine("How old are you? Round numbers only, please.");
-            int age = int.Parse(Console.ReadLine());
+            string ageText = Console.ReadLine();
+            //might need to accept a string then convert to int?  maybe try the answer as an if/else
+            if (ageText.ToLower() == "quit")
+            {
+                Console.WriteLine("Nobody likes a quitter!");
+                return;
+            }
+
+            int age = Convert.ToInt32(ageText);
 
             Console.WriteLine("In what month were you born? Please use the number... January is 1, etc.");
-            int month = int.Parse(Console.ReadLine());
+            string monthText = Console.ReadLine();
+            //same problem
+            if (monthText.ToLower() == "quit")
+            {
+                Console.WriteLine("Nobody likes a quitter!");
+                return;
+            }
+
+            int month = Convert.ToInt32(monthText);
 
             Console.WriteLine("How many siblings do you have?");
-            int sibs = int.Parse(Console.ReadLine());
+            string sibsText = Console.ReadLine();
+            //same problem
+            if (sibsText.ToLower() == "quit")
+            {
+                Console.WriteLine("Nobody likes a quitter!");
+                return;
+            }
 
-            //tricky part... get favorite color
+            int sibs = Convert.ToInt32(sibsText);
+
+            //get favorite color
             //offer option for help
             //make not case sensitive
 
@@ -39,16 +76,30 @@ namespace FortuneTellerProject
             string colorChoice = Console.ReadLine().ToLower();
             string colorHelp = "help";
 
-            if (colorChoice == colorHelp /*|| colorChoice == colorHelp.ToUpper() || colorChoice == colorHelp.ToLower()*/)
+            if (colorChoice == colorHelp)
             {
                 Console.WriteLine("The Roy G. Biv colors are red, orange, yellow, green, blue, indigo, violet.");
                 Console.WriteLine("Now that you know Roy G. Biv, what is your favorite color?");
                 colorChoice = Console.ReadLine().ToLower();
             }
 
+            //same problem
+            if (colorChoice == "quit")
+            {
+                Console.WriteLine("Nobody likes a quitter!");
+                return;
+            }
+            
             Console.WriteLine($"So you like {colorChoice}, eh? Me too.");
             Console.WriteLine("\nIf you are ready for your fortune, press enter...\n");
+            string possQuitPlace1 = Console.ReadLine();
 
+            if (possQuitPlace1 == "quit")
+            {
+                Console.WriteLine("Nobody likes a quitter!");
+                return;
+            }
+            
             //predict years until retirement from age
 
             int retYears;
@@ -64,15 +115,86 @@ namespace FortuneTellerProject
 
             //predict vacation home from number of sibs
 
-            string vacayHome;
-            switch (sibs)
-            {
-                case 0:
-                    vacayHome = "Put-in-Bay"
-            }
-            
+            string vacayHome = "";
 
-            //string secondColorChoice = 
+            if (sibs < 0)
+            {
+                vacayHome = "Aleppo, Syria";
+            }
+            else if (sibs == 0)
+            {
+                vacayHome = "Put-in-Bay";
+            }
+            else if (sibs == 1)
+            {
+                vacayHome = "The Rocky Mountains";
+            }
+            else if (sibs == 2)
+            {
+                vacayHome = "Key West";
+            }
+            else if (sibs >= 3)
+            {
+                vacayHome = "London, England";
+            }
+
+            //predictions about trandportation and colors
+
+            string transport = "";
+
+            switch (colorChoice)
+            {
+                case "red":
+                    transport = "car";
+                    break;
+                case "orange":
+                    transport = "pogo stick";
+                    break;
+                case "yellow":
+                    transport = "bicycle";
+                    break;
+                case "green":
+                    transport = "spaceship";
+                    break;
+                case "blue":
+                    transport = "boat";
+                    break;
+                case "indigo":
+                    transport = "skateboard";
+                    break;
+                case "violet":
+                    transport = "Harley Davidson";
+                    break;
+            }
+
+            //dollars in bank and birth month predictions
+
+            double dollars;
+
+            if (month >= 1 && month <= 4)
+            {
+                dollars = 25000;
+            }
+            else if (month >=5 && month <= 8)
+            {
+                dollars = 300000;
+            }
+            else if (month >=9 && month <= 12)
+            {
+                dollars = 6.37;
+            }
+            else
+            {
+                dollars = 0;
+            }
+
+            //final predictions
+
+            Console.WriteLine($"{firstName} {lastName}, you will retire in {retYears} years " + 
+                $"with ${dollars} in the bank, \na vacation home in {vacayHome} " +
+                $"and a {transport}.");
+            Console.WriteLine("\nOh, and make sure you look both ways before you cross the street today.");
+
             Console.ReadLine();
         }
     }
